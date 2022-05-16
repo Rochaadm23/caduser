80da5b118b021f2549813b6b52598e0f032aa6ab
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2022-05-16 16:34:34 -0300

    User update

--- src/main/java/com/rsecinformation/caduser/services/UserService.java
+++ src/main/java/com/rsecinformation/caduser/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
     public void delete(Long id){
         userRepositorie.deleteById(id);
     }
+
+    public User update(Long id, User obj){
+        User entity = userRepositorie.getOne(id);
+        updateData(entity, obj);
+        return userRepositorie.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
 }
