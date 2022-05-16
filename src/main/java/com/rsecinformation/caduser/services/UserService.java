ca4c028ed341f27486c490c550de3a24bc8aab20
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2022-05-16 14:54:31 -0300

    Camada de serviço e registro de componente

---
+++ src/main/java/com/rsecinformation/caduser/services/UserService.java
@@ -0,0 +1,27 @@
+package com.rsecinformation.caduser.services;
+
+import com.rsecinformation.caduser.entities.User;
+import com.rsecinformation.caduser.repositories.UserRepositorie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepositorie userRepositorie;
+
+    public List<User> findAll(){
+        return userRepositorie.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = userRepositorie.findById(id);
+        return obj.get();
+    }
+
+}
