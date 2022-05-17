daf233aeb75a7baa31a8f8568127c8fa8adc290f
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2022-05-17 00:27:22 -0300

    Exception handle - update

--- src/main/java/com/rsecinformation/caduser/services/UserService.java
+++ src/main/java/com/rsecinformation/caduser/services/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.lang.module.ResolutionException;
 import java.util.List;
 import java.util.Optional;
@@ -39,15 +40,19 @@ public class UserService {
             userRepositorie.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(id);
-        }catch (DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             throw new DatabaseException(e.getMessage());
         }
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepositorie.getById(id);
-        updateData(entity, obj);
-        return userRepositorie.save(entity);
+        try {
+            User entity = userRepositorie.getById(id);
+            updateData(entity, obj);
+            return userRepositorie.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
